a02edeaaa18613bebdd8fcf6c0d718372444127c
Gabriel Martins <gabriel_alves_m@outlook.com.br>
2021-07-07 12:18:51 -0300

    Created input

--- OddEven.py
+++ OddEven.py
@@ -1,14 +1,17 @@
-all = []
+all_x = []
 odd = []
 even = []
 
+#Array size
+x = int(input('Array size = '))
+
 #Creating list
-for i in range(25):
+for i in range(x):
   i += 1
-  all.append(i)
+  all_x.append(i)
 
 #Separating lists
-for i in all:
+for i in all_x:
   if i % 2 != 0:
     odd.append(i)
   else:
