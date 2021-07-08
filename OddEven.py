6ae3aeaa8c6355178dd261d4448589ae031382f4
Gabriel Martins <gabriel_alves_m@outlook.com.br>
2021-07-08 07:59:19 -0300

    Update OddEven.py

--- OddEven.py
+++ OddEven.py
@@ -5,17 +5,16 @@ even = []
 #Array size
 x = int(input('Array size = '))
 
-#Creating list
 for i in range(x):
   i += 1
+  #Creating list
   all_x.append(i)
 
-#Separating lists
-for i in all_x:
+  #Separating lists
   if i % 2 != 0:
     odd.append(i)
   else:
-    even.append(i)
+    even.append(i)  
 
 print(odd)
 print(even)
